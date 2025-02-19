8e62788393f8cd99da26d67bb248506641f25533
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-02-19 09:15:20 +0800

    feat: cookie

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ urllib3.disable_warnings()
 
 hd = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76",
+    "cookie": "",  # 可能需要携带Cookies，否则可能出现-352错误
 }
 
 
